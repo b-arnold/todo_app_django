d108eff1475ac024e72bb9ce9938edc202b60ea5
Brett Arnold <brett.arnold@qmerit.com>
2020-08-16 15:59:20 -0700

    finally client is setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+interface User {
+  email: string;
+}
 
 function App() {
+  const [user, setUser] = useState<User>({ email: "" });
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUser(data[0]);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>User: {user && user.email}</p>
     </div>
   );
 }
